e0e76a40fcef1a6142fd290d5ebb63a7c98db76c
Nirigoyen <116807507+Nirigoyen@users.noreply.github.com>
2023-07-04 20:56:35 -0300

    Agregado de Gemas

--- src/main/java/net/n1korasu/pruebamod/item/ModItems.java
+++ src/main/java/net/n1korasu/pruebamod/item/ModItems.java
@@ -15,6 +15,15 @@ public class ModItems {
     public static final RegistryObject<Item> CHIPGRANDE = ITEMS.register("chipgrande", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
 
 
+    //GEMAS
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
+    public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
+    public static final RegistryObject<Item> GARNET = ITEMS.register("garnet", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
+    public static final RegistryObject<Item> SMOKESTONE = ITEMS.register("smokestone", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
+    public static final RegistryObject<Item> TOPAZ = ITEMS.register("topaz", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
+    public static final RegistryObject<Item> HELIODOR = ITEMS.register("heliodor", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
+
 
     public static void register (IEventBus eventBus){
         ITEMS.register(eventBus);
