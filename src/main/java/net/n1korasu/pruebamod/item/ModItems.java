4f516f8d9e29ca1d4413ba2136446d353cb6ec62
Nirigoyen <116807507+Nirigoyen@users.noreply.github.com>
2023-07-08 00:12:57 -0300

    Creado de Espada de prueba

--- src/main/java/net/n1korasu/pruebamod/item/ModItems.java
+++ src/main/java/net/n1korasu/pruebamod/item/ModItems.java
@@ -2,6 +2,10 @@ package net.n1korasu.pruebamod.item;
 
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -23,7 +27,20 @@ public class ModItems {
     public static final RegistryObject<Item> SMOKESTONE = ITEMS.register("smokestone", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
     public static final RegistryObject<Item> TOPAZ = ITEMS.register("topaz", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
     public static final RegistryObject<Item> HELIODOR = ITEMS.register("heliodor", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
+    //public static final RegistryObject<Item> SWORD = ITEMS.register("heliodor", () -> new SwordItem(new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
 
+    public static final RegistryObject<SwordItem> SWORD2 = ITEMS.register("sword", () -> new SwordItem(Tiers.Prueba,0,-3.8f, new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
+
+
+
+    public static Item.Properties propsTab(){
+        return new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB);
+    }
+
+    public static class Tiers{
+        public static final Tier Prueba = new ForgeTier(0, Integer.MAX_VALUE, -3, 10, 0, null, () -> Ingredient.EMPTY);
+
+    }
 
     public static void register (IEventBus eventBus){
         ITEMS.register(eventBus);
