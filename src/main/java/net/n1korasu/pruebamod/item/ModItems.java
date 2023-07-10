ad0b09b8bf7f80547998ebeecc592c031163e94d
Nirigoyen <116807507+Nirigoyen@users.noreply.github.com>
2023-07-10 19:49:16 -0300

    Revert "Push temporal"
    This reverts commit ffecf2115df9361f52078e5927ac206d574c17b4.

--- src/main/java/net/n1korasu/pruebamod/item/ModItems.java
+++ src/main/java/net/n1korasu/pruebamod/item/ModItems.java
@@ -3,8 +3,6 @@ package net.n1korasu.pruebamod.item;
 
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
@@ -37,12 +35,6 @@ public class ModItems {
     //ESPADAS
     public static final RegistryObject<SwordItem> SWORD2 = ITEMS.register("sword", () -> new Oathbringer(net.n1korasu.pruebamod.item.custom.Tiers.Esquirlada,0,-3.8f, new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
 
-
-    //ARMADURA
-    public static final RegistryObject<Item> SHARD_HELMET = ITEMS.register("shardHelmet", () -> new ArmorItem(ModArmorMaterials.ESQUIRLADA, EquipmentSlot.HEAD, new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
-    public static final RegistryObject<Item> SHARD_CHEST = ITEMS.register("shardChest", () -> new ArmorItem(ModArmorMaterials.ESQUIRLADA, EquipmentSlot.CHEST, new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
-    public static final RegistryObject<Item> SHARD_LEGGINGS = ITEMS.register("shardLeggings", () -> new ArmorItem(ModArmorMaterials.ESQUIRLADA, EquipmentSlot.LEGS, new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
-    public static final RegistryObject<Item> SHARD_BOOTS = ITEMS.register("shardBoots", () -> new ArmorItem(ModArmorMaterials.ESQUIRLADA, EquipmentSlot.FEET, new Item.Properties().tab(ModCreativeModeTab.PRUEBA_TAB)));
 
     public static void register (IEventBus eventBus){
         ITEMS.register(eventBus);
