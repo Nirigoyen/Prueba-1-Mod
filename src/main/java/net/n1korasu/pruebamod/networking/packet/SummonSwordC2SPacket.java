0ba295223c3a312b3d4253f1a0bd08e6f6792637
Nirigoyen <116807507+Nirigoyen@users.noreply.github.com>
2023-07-14 16:54:56 -0300

    Update SummonSwordC2SPacket.java

--- src/main/java/net/n1korasu/pruebamod/networking/packet/SummonSwordC2SPacket.java
+++ src/main/java/net/n1korasu/pruebamod/networking/packet/SummonSwordC2SPacket.java
@@ -5,6 +5,7 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.event.RegisterColorHandlersEvent;
@@ -41,21 +42,11 @@ public class SummonSwordC2SPacket {
             //CHECKEAR QUE EL PLAYER TENGA LA MANO VACIA
 
 
-            if (player.getMainHandItem().isEmpty()){
+            if (player.getMainHandItem().isEmpty() && !player.getInventory().contains(new ItemStack(ModItems.OATHBRINGER.get()))){
                 summon(player);
-
-                //player.getInventory().add(new ItemStack(Items.STICK));
-
-                player.getInventory().add(new ItemStack(ModItems.OATHBRINGER.get()));
-
-//                player.addItem(new ItemStack(Items.STICK)); //ME DA EL ITEM PERO NO EN FORMATO DE ESPADA, SOLAMENTE COMO UN ITEM CUALQUIERA
-
-
-
-
+                ItemStack item = new ItemStack(ModItems.OATHBRINGER.get());
+                player.getInventory().add(item);
             }
-
-
         });
         return true;
     }
@@ -63,7 +54,7 @@ public class SummonSwordC2SPacket {
     public void summon(ServerPlayer player){
         player.sendSystemMessage(Component.literal("10 heartbeats..."));
         for (int i = 9; i > 0; i-- ){
-            Minecraft.getInstance().player.sendSystemMessage(Component.literal(i + "..."));
+            player.sendSystemMessage(Component.literal(i + "..."));
         }
     }
 
